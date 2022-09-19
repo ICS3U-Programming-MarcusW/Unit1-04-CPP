19e00c6525f68dbe5613148668f69864ec8726da
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2022-09-19 00:26:31 +0000

    Updated program

--- areaPerRect.cpp
+++ areaPerRect.cpp
@@ -10,8 +10,8 @@
 int main() {
     std::cout << "Area and perimeter of a rectangle - " << std::endl;
     std::cout << "If a rectangle has the dimensions:" << std::endl;
-    std::cout << "7cm x 13cm" << std::endl;
+    std::cout << "17cm x 9cm" << std::endl;
     std::cout << " " << std::endl;
-    std::cout << "The area is: " << (7 * 13) << "cm^2" << std::endl;
-    std::cout << "The perimeter is: " << (2 * (7 + 13)) << "cm" << std::endl;
+    std::cout << "The area is: " << (17 * 9) << "cm^2" << std::endl;
+    std::cout << "The perimeter is: " << (2 * (17 + 9)) << "cm" << std::endl;
 }
