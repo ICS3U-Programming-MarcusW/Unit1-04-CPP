41ff93ce631375e3c42cadbfac0e148fa6b24517
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2022-09-19 00:17:36 +0000

    Created program to calculate area and perimeter of rectangle  in c++

---
+++ areaPerRect.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Marcus Wehbi All rights reserved
+
+// Created by: Marcus Wehbi
+// Created on: Sep 18
+// This program calculates area and perimeter of a rectangle in c++
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "Area and perimeter of a rectangle - " << std::endl;
+    std::cout << "If a rectangle has the dimensions:" << std::endl;
+    std::cout << "7cm x 13cm" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "The area is: " << (7 * 13) << "cm^2" << std::endl;
+    std::cout << "The perimeter is: " << (2 * (7 + 13)) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Marcus Wehbi All rights reserved
+
+// Created by: Marcus Wehbi
+// Created on: Sep 18
+// This program does basic math in c++
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "Addition: 23 + 76 = " << (23 + 76) << std::endl;
+    std::cout << "Subtraction: 111 - 45 = " << (111 - 45) << std::endl;
+    std::cout << "Multiplication: 23.5 * 9.22 = " << (23.5 * 9.22) << std::endl;
+    std::cout << "Division: 31 / 3 = " << (31 / 3) << std::endl;
+    std::cout << "Powers: 2 ^ 4 = " << (pow(2, 4)) << std::endl;
+    std::cout << "Roots: 81 √  9 = " << (sqrt(81)) << std::endl;
+}
